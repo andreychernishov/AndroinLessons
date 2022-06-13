78897c3c645829a5782d9ebc3267773889574173
andreychernishov <andruha46@gmail.com>
2022-06-13 12:00:54 +0300

    l23 start, clear all

--- app/src/main/java/com/example/lesson19/MainActivity.kt
+++ app/src/main/java/com/example/lesson19/MainActivity.kt
@@ -8,12 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var user = User("Sergey","222222",12)
-        var user1 = User("Andrii","121212",32)
-        user.addAge(11)
-        user1.addAge(1)
-        user.printInfo()
-        user1.printInfo()
+        
     }
 }

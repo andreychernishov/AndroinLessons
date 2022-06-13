dca954914b2b7873f51bf7982d4272c70053e8f9
andreychernishov <andruha46@gmail.com>
2022-06-13 11:57:23 +0300

    l22 start created class User add constructor add fun addAge, printInfo

--- app/src/main/java/com/example/lesson19/MainActivity.kt
+++ app/src/main/java/com/example/lesson19/MainActivity.kt
@@ -9,6 +9,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        var user = User("Sergey","222222",12)
+        var user1 = User("Andrii","121212",32)
+        user.addAge(11)
+        user1.addAge(1)
+        user.printInfo()
+        user1.printInfo()
     }
 }
--- app/src/main/java/com/example/lesson19/User.kt
+++ app/src/main/java/com/example/lesson19/User.kt
@@ -1,4 +1,14 @@
 package com.example.lesson19
 
-class User {
+import android.util.Log
+
+
+class User( var name: String, var id: String, var age: Int) {
+
+    fun addAge(years: Int){
+        age += years
+    }
+    fun printInfo(){
+        Log.d("@@@","User name: $name, id: $id, age: $age")
+    }
 }

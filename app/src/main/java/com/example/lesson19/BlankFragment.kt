bacb256c59b8e64bc6aeb99ebbde9eed6e338782
Andrii Chernyshov <andruha46@gmail.com>
2022-09-24 16:38:15 +0300

    l33. sending between fragments

--- app/src/main/java/com/example/lesson19/BlankFragment.kt
+++ app/src/main/java/com/example/lesson19/BlankFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
 
 class BlankFragment : Fragment() {
+
+    private val dataModel : DataModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -15,7 +20,26 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var buttonSend: Button = requireView().findViewById(R.id.fragment_1_btn_1)
+        var btnSendToActivity: Button = requireView().findViewById(R.id.fragment_1_btn_2)
+        var frag1Tw : TextView = requireView().findViewById(R.id.fragment_1_tw)
+        dataModel.messageForFrag1.observe(activity as LifecycleOwner,{
+            frag1Tw.text = it
+        })
+
+        buttonSend.setOnClickListener {
+            dataModel.messageForFrag2.value = "Hello Fragment 2 from fragment 1"
+        }
+        btnSendToActivity.setOnClickListener {
+            dataModel.messageForActivity.value = "Hello Activity from fragment 1"
+        }
+
     }
 
     companion object {
@@ -23,4 +47,6 @@ class BlankFragment : Fragment() {
         @JvmStatic
         fun newInstance() = BlankFragment()
     }
+
+
 }
--- app/src/main/java/com/example/lesson19/MainActivity.kt
+++ app/src/main/java/com/example/lesson19/MainActivity.kt
@@ -2,28 +2,37 @@ package com.example.lesson19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 
+ class MainActivity : AppCompatActivity() {
+    private val dataModel : DataModel by viewModels()
+    private lateinit var mainTw : TextView
+    private lateinit var frag1 : TextView
+    private lateinit var frag2 : TextView
 
-import com.example.lesson19.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         val button : Button = findViewById(R.id.btn1)
+        init()
 
-        button.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frame_id, BlankFragment2.newInstance())
-                .commit()
-        }
+        openFrag(BlankFragment.newInstance(), R.id.frame_id)
+        openFrag(BlankFragment2.newInstance(),R.id.frame_2_id)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frame_id,BlankFragment.newInstance())
-            .commit()
+        dataModel.messageForActivity.observe(this) { mainTw.text = it }
+        /*dataModel.messageForFrag1.observe(this){frag1.text = it}
+        dataModel.messageForFrag2.observe(this){frag2.text = it }
+ */
+    }
+
+    private fun openFrag(f : Fragment, idHolder: Int){
+        supportFragmentManager.beginTransaction().replace(idHolder,f).commit()
+    }
+    private fun init(){
+         mainTw = findViewById(R.id.hello_world_tw)
 
     }
+
 }
